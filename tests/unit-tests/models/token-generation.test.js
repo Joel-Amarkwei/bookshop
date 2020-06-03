d2fc7b4b0a728fab712c3511465cefe5e420aa62
Joel-Amarkwei <61187611+Joel-Amarkwei@users.noreply.github.com>
2020-06-03 16:38:51 +0000

    Update token-generation.test.js

--- tests/unit-tests/models/token-generation.test.js
+++ tests/unit-tests/models/token-generation.test.js
@@ -1,8 +1,11 @@
+//based on my code format, this property inside is probalbly the most suitable for unit testing.
+
 const { User } = require('../../../models/users')
 const jwt = require('jsonwebtoken')
 const mongoose = require('mongoose')
 const config = require('config')
 
+// this test just checks the efficiency of the token genetator function I created within the User's model
 describe('tokenGenerator', () => {
     it('should return a json web-token', async () => {
         const payload = {
@@ -15,3 +18,10 @@ describe('tokenGenerator', () => {
         expect(decode).toMatchObject(payload)
     })
 })
+
+/*
+Things to do before you can run these tests,...
+-   Install the following node modules -> jwt, mongoose, config
+-   Require User from the users model.
+-   You can other test cases and see how they work out.
+*/
